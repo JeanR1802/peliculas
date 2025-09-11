7d221b08e1847d677cdd192fd2e358380dfd54b4
JeanR1802 <rs.cedora@gmail.com>
2025-09-11 03:39:09 -0700

    Conectar cliente al servidor de Render

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import './App.css';
 
 // Conectamos con nuestro servidor
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("[https://modixia-watch-party-server.onrender.com](https://modixia-watch-party-server.onrender.com)"); // <-- Pega tu URL de Render
 
 function App() {
   const [roomId, setRoomId] = useState('mi-sala-secreta');
